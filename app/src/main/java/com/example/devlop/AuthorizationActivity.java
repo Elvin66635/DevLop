4ea1a218d045ec71684b8f49e5866545eb11d93a
Elvin <53175847+Elvin66635@users.noreply.github.com>
2020-09-05 12:19:00 +0400

    Firebase

--- app/src/main/java/com/example/devlop/AuthorizationActivity.java
+++ app/src/main/java/com/example/devlop/AuthorizationActivity.java
@@ -1,27 +1,35 @@
 package com.example.devlop;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class AuthorizationActivity extends AppCompatActivity {
     TextView forgotPassTxt, signUpTxt;
     EditText edLogin, edPass;
     Button signInBtn;
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authorization);
-
+        mAuth = FirebaseAuth.getInstance();
         initView();
     }
 
@@ -39,7 +47,40 @@ public class AuthorizationActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String pass = edPass.getText().toString().trim();
+                String username = edLogin.getText().toString().trim();
+
+                if (TextUtils.isEmpty(pass)) {
+                    edPass.setError("Пароль пустой");
+                    return;
+                }
+                if (pass.length() < 6){
+                    edPass.setError("Пароль должен быть больше 6-ти символов");
+                }
+                if (TextUtils.isEmpty(username)) {
+                    edLogin.setError("Имя пользователя пустое");
+                    return;
+                }
+                mAuth.signInWithEmailAndPassword(username, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Пользователь создан", Toast.LENGTH_LONG).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Ошибка " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            }
+        });
     }
+
+
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
--- app/src/main/java/com/example/devlop/RegistrationActivity.java
+++ app/src/main/java/com/example/devlop/RegistrationActivity.java
@@ -3,6 +3,7 @@ package com.example.devlop;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -10,38 +11,96 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegistrationActivity extends AppCompatActivity {
     private static final String TAG = "RegistrationActivity";
-    EditText edLoginReg, edPassReg;
+    EditText edLoginReg, edPassReg, edEmailReg;
     Button signUpBtn;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
+        mAuth = FirebaseAuth.getInstance();
+
         initView();
+
+        if (mAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
     }
 
     private void initView() {
         edLoginReg = findViewById(R.id.edLoginReg);
         edPassReg = findViewById(R.id.edPassReg);
+        edEmailReg = findViewById(R.id.edEmailReg);
         signUpBtn = findViewById(R.id.signUpBtn);
-        
+
         signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String email = edEmailReg.getText().toString().trim();
+                String pass = edPassReg.getText().toString().trim();
+                String username = edLoginReg.getText().toString().trim();
 
+                if (TextUtils.isEmpty(email)) {
+                    edEmailReg.setError("Email пустой");
+                    return;
+                }
+                if (TextUtils.isEmpty(pass)) {
+                    edPassReg.setError("Пароль пустой");
+                    return;
+                }
+                if (pass.length() < 6){
+                    edPassReg.setError("Пароль должен быть больше 6-ти символов");
+                }
+                if (TextUtils.isEmpty(username)) {
+                    edLoginReg.setError("Имя пользователя пустое");
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(RegistrationActivity.this, "Пользователь создан", Toast.LENGTH_LONG).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(RegistrationActivity.this, "Ошибка " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
             }
+
+
         });
     }
 
     @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //    updateUI(currentUser);
+    }
+
+    @Override
     public boolean onTouchEvent(MotionEvent event) {
-        InputMethodManager imm = (InputMethodManager)getSystemService(Context.
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.
                 INPUT_METHOD_SERVICE);
         imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
         return true;
